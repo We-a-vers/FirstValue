391bdb006faaa764367a5cc46df669e4ac3dea05
BenjaminnHuang <peternick0207@gmail.com>
2023-11-15 05:28:07 -0800

    update the router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home/home_page"
+import About from "./pages/About/about_page"
+import ServiceTech from "./pages/ServiceTech/service_page"
+import Navbar from "./components/Navbar"
 
-function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar/>}>
+      <Route index element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="service" element={<ServiceTech/>}/>
+    </Route>
+  )
+)
+
+const App = () => {
   return (
-    <>
-      test
-    </>
+    <RouterProvider router={router }/>
   )
 }
 
