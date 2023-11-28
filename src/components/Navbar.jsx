4fe9f81aa23c56a7bf67fe1e1fea819ba9be1416
Andy Lin <76961516+AndyLin910121@users.noreply.github.com>
2023-11-28 00:07:12 -0800

    style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,37 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const Navbar = () => {
-  const navStyle = {
-    background: '#7B8EA6',
-    color: '#FFFFFF',
-    padding: '20px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  };
+  // const navStyle = {
+  //   background: '#7B8EA6',
+  //   color: '#FFFFFF',
+  //   padding: '20px',
+  //   display: 'flex',
+  //   flexDirection: 'row',
+  //   justifyContent: 'space-between',
+  //   alignItems: 'center',
+  // };
 
-  const ulStyle = {
-    listStyle: 'none',
-    display: 'flex',
-    justifyContent: 'flex-end',
-  };
+  // const ulStyle = {
+  //   listStyle: 'none',
+  //   display: 'flex',
+  //   justifyContent: 'flex-end',
+  // };
 
-  const liStyle = {
-    margin: '0 10px',
-  };
+  // const liStyle = {
+  //   margin: '0 10px',
+  // };
 
   return (
     <div>
-        <div style={navStyle} className="navbar">
-            <div class="font-medium">鴻日興科技 First Value Technology Co</div>
-            <ul style={ulStyle} className="nav-list">
-                <NavLink to="/"><li style={liStyle}>主頁面</li></NavLink>
-                <NavLink to="about"><li style={liStyle}>關於我們</li></NavLink>
-                <NavLink to="service"><li style={liStyle}>技術</li></NavLink>
-                <li style={liStyle}>服務</li>
-                <li style={liStyle}>聯絡我們</li>
-                <li style={liStyle}>中文 </li>
+        <div className="bg-white text-blue p-4 flex justify-between center ">
+            <div className="font-medium">鴻日興科技 First Value Technology Co</div>
+            <ul  className="flex list-none justify-end items-center gap-2">
+                <NavLink to="/"><li >主頁面</li></NavLink>
+                <NavLink to="about"><li >關於我們</li></NavLink>
+                <NavLink to="service"><li >技術</li></NavLink>
+                <li >服務</li>
+                <li >聯絡我們</li>
+                <li >中文 </li>
             </ul>
         </div>
         
