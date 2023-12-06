c5508ad1973b881d3abfafadd4eba3d032f7119e
joannePon22 <jpon@ucsd.edu>
2023-12-05 19:58:49 -0800

    update servicepage

--- src/pages/ServiceTech/service_page.jsx
+++ src/pages/ServiceTech/service_page.jsx
@@ -1,17 +1,11 @@
 import Header from "./components/Header";
 
 const ServiceTech = () => {
-<<<<<<< HEAD
-    return (
-        <>
-            <Header/ >
-        </>
-
-    )
-}
-=======
-  return <>ServiceTech</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
->>>>>>> 784bbad43c75aa78d2f10c255b8bac98fab60cac
 
 export default ServiceTech;
