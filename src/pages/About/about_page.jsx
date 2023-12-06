6e0b6f57f94fc32e197867255053aeb052423ea1
Shy, Nicholas <nshy@ucsd.edu>
2023-12-05 20:01:53 -0800

    changed to tablet/desktop

--- src/pages/About/about_page.jsx
+++ src/pages/About/about_page.jsx
@@ -1,4 +1,4 @@
-import Timeline from "./components/timeline";
+import Timeline from "./components/time_line";
 
 const About = () => {
   const outerFrameStyle = {};
@@ -18,62 +18,7 @@ const About = () => {
         <div className="w-60 tablet:w-auto text-zinc-800 text-xs desktop:text-[28px] font-normal font-['Noto Sans TC'] leading-tight desktop:tracking-[2.80px] pt-5">
           滿足客戶的製程需求的同時，也努力將技術做現地化以利長久的技術運作。
         </div>
-      </header>
-      {/* timeline */}
-      <div className="pl-[30px]">
-        <div className="relative desktop:h-full desktop:w-8/12 desktop:pt-0 bg-gradient-to-l from-blue-950 to-blue-300 ">
-          <div class="desktop:w-full desktop:pl-7 desktop:h-fit pb-2 bg-white text-zinc-800 text-[42px] font-bold font-['Inter']">
-            2012
-          </div>
-          <div className="desktop:w-[99.5%] desktop:h-48 desktop:pl-7 desktop:mt-1 pt-2 bg-white">
-            與美國合作銅薄膜真空蝕刻Sputter製程
-          </div>
-          <div className="text-right desktop:w-[99.5%] desktop:pr-10 desktop:h-fit pb-2 bg-white text-zinc-800 text-[42px] font-bold font-['Inter']">
-            2013
-          </div>
-          <div className="text-right ml-auto desktop:w-[99.5%] desktop:h-44 desktop:pr-10 desktop:mt-1 pt-2 bg-white">
-            與烏克蘭合作在中國現地化自製藍寶石
-            <br />
-            長晶爐銷售及相關藍寶石材料銷售
-          </div>
-          <div class="desktop:w-[99.5%] ml-auto desktop:pl-7 desktop:h-fit pb-3 bg-white text-zinc-800 text-[42px] font-bold font-['Inter']">
-            2014
-          </div>
-          <div className="desktop:w-[99.5%] desktop:h-48 desktop:pl-7 desktop:mt-1 pt-2 bg-white">
-            成立大陸辦事處,開發測試治具及探針應用
-          </div>
-          <div className="text-right desktop:w-[99.5%] desktop:pr-10 desktop:h-fit pb-2 bg-white text-zinc-800 text-[42px] font-bold font-['Inter']">
-            2015
-          </div>
-          <div className="text-right ml-auto desktop:w-[99.5%] desktop:h-40 desktop:pr-10 desktop:mt-1 pt-2 bg-white">
-            成功開發銷售UV固化燈及高亮度檢查燈 及封裝測試用擴晶環及自動
-            <br />
-            精密點膠機搭配CCD視覺判別，真空Pump/ motor/plasma/壓力計等
-            <br />
-            零件維修服務
-          </div>
-          <div class="desktop:w-[99.5%] ml-auto desktop:pl-7 desktop:h-fit pb-3 bg-white text-blue-950 text-4xl font-medium font-['Inter']">
-            2017
-          </div>
-          <div className="desktop:w-[99.5%] desktop:h-48 desktop:pl-7 desktop:mt-1 pt-2 bg-white">
-            ITO靶材燒結爐 with 日本 ；3D貼合治具改善0.1mmt玻璃吸附問題
-          </div>
-          <div className="text-right desktop:w-[99.5%] desktop:pr-10 desktop:h-fit pb-2 bg-white text-zinc-800 text-[42px] font-bold font-['Inter']">
-            2018
-          </div>
-          <div className="text-right ml-auto desktop:w-[99.5%] desktop:h-32 desktop:pr-10 desktop:mt-1 pt-2 bg-white">
-          開發切割精度檢查機及超高亮度面光<br />源、Micro Led貼合治具
-          </div>
-          <div class="desktop:w-[99.5%] ml-auto desktop:pl-7 desktop:h-fit pb-3 bg-white text-blue-950 text-4xl font-bold font-['Inter']">
-            2020-2022
-          </div>
-          <div class="desktop:w-[99.5%] ml-auto desktop:pl-7 desktop:h-fit bg-white text-blue-950 font-medium font-['Inter']">
-          將提升機械手臂視覺的系統規劃及自動化設備開發
-          </div>
-        </div>
-      </div>
-
-      <div></div>
+      </header> 
     </div>
   );
 };
