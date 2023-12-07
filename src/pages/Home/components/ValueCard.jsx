f034b89fd8170de8b16079ecab844ed2cb6e82f2
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2023-12-06 17:18:47 -0800

    Implement PropTypes, Modify Tailwind.Config, Implement Value Card based on final design

--- src/pages/Home/components/ValueCard.jsx
+++ src/pages/Home/components/ValueCard.jsx
@@ -1,20 +1,39 @@
-const ValueCard = ({Icon, Word}) => {
-    return (
-        <div className="flex w-72 h-20 gap-2.5 justify-center
-                        tablet:flex-start tablet:w-valueTabletWidth
-                        desktop:flex-col desktop:gap-valueGap desktop:w-valueWidth desktop:h-valueHeight">
-            <div className="w-20 h-20
-                            tablet:w-8 tablet:h-8
-                            desktop:w-24 desktop:h-24">
-                <img src={Icon} alt="value icon"></img>
-            </div>
-            <div className="text-sm 
-                            tablet:w-valueTabletWordsWidth
-                            desktop:text-xl desktop:w-valueWidth">
-                {Word}
-            </div>
-        </div>    
-    )   
-}
+import PropTypes from 'prop-types';
 
-export default ValueCard
+const ValueCard = ({ icon, word }) => {
+  return (
+    <div
+      className="flex justify-center items-center w-40 h-36 rounded-xl shadow-value-card backdrop-blur-sm px-3.5 py-6
+                    tablet:w-48 tablet:h-44 tablet:px-4 tablet:py-6
+                    desktop:w-96 desktop:h-64 desktop:px-10 desktop:py-10"
+    >
+      <div
+        className="flex flex-col gap-3 justify-center items-start w-36 h-32
+                            tablet:flex-start tablet:gap-3.5 tablet:w-40 tbablet:h-36
+                            desktop:gap-4 desktop:w-auto desktop:h-auto"
+      >
+        <div
+          className=" w-8 h-8
+                            tablet:w-10 tablet:h-10
+                            desktop:w-valueImgSizeDesk desktop:h-valueImgSizeDesk"
+        >
+          <img src={icon} alt="value icon"></img>
+        </div>
+        <div
+          className=" font-chi-sans text-[0.6875rem] not-italic font-normal leading-4
+                            tablet:text-xs
+                            desktop:text-xl desktop:leading-6"
+        >
+          {word}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ValueCard.propTypes = {
+  icon: PropTypes.any.isRequired,
+  word: PropTypes.string.isRequired,
+};
+
+export default ValueCard;
