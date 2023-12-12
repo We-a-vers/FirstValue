fe05c59a69b90f8810d3bd761f960ed9553c23d0
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2023-12-12 15:29:44 -0800

    Add tailwind styling for tablet and desktop styling

--- src/pages/Home/components/ServiceTechCard.jsx
+++ src/pages/Home/components/ServiceTechCard.jsx
@@ -2,24 +2,42 @@ import PropTypes from 'prop-types';
 
 const ServiceTechCard = ({ image, title, description }) => {
   return (
-    <div className="flex flex-col justify-center items-center w-[19rem] h-auto p-[1.875rem] gap-[1.875rem] bg-natural-color-white shadow-service-tech-card-border rounded-[0.625rem] border border-solid border-service-tech-card-border-color">
+    <div
+      className="flex flex-col justify-center items-center w-[19rem] h-auto gap-[1.875rem] max-tablet:p-[1.875rem] max-tablet:bg-natural-color-white max-tablet:shadow-service-tech-card-border max-tablet:rounded-[0.625rem] max-tablet:border max-tablet:border-solid max-tablet:border-service-tech-card-border-color
+                    tablet:gap-[0.75rem] tablet:items-start
+                    desktop:gap-[2.1875rem] desktop:w-[34.375rem] desktop:shrink-0"
+    >
       <div>
         <img
-          className="flex flex-column items-start shrink-0 w-[15.25rem] h-[9.3125rem] rounded-lg"
+          className="flex flex-column items-start shrink-0 w-[15.25rem] h-[9.3125rem] rounded-lg
+                    tablet:w-[19rem] tablet:h-[10.3rem]
+                    desktop:w-[34.375rem] desktop:h-[18.75rem]"
           src={image}
           alt="service tech image"
         ></img>
       </div>
-      <div className="flex flex-col items-start gap-[0.5rem]">
-        <div className="font-chi-sans text-[0.875rem] not-italic font-medium text-foundation-blue-normal leading-[110%]">
+      <div
+        className="flex flex-col items-start gap-[0.5rem] 
+      tablet:gap-[0.7rem] tablet:pb-[1.02rem] tablet:shrink-0 tablet:self-stretch "
+      >
+        <div
+          /* Font changes? */
+          className="font-chi-sans text-[0.875rem] not-italic font-medium text-foundation-blue-normal leading-[110%]
+                        tablet:font-chi-serif tablet:text-[1.125rem] tablet:font-normal tablet:text-cool-grey-90 tablet:self-stretch
+                        desktop:font-chi-sans desktop:text-[2rem] desktop:font-medium desktop:text-foundation-blue-normal"
+        >
           {title}
         </div>
-        <div className="self-stretch font-chi-sans text-[0.6875rem] not-italic font-normal text-cool-grey-90 leading-[140%]">
+        <div
+          className="self-stretch font-chi-sans text-[0.6875rem] not-italic font-normal text-cool-grey-90 leading-[140%]
+                        tablet:text-[0.75rem] tablet:max-desktop:w-[19.75rem] 
+                        desktop:text-[1.25rem]"
+        >
           {description}
         </div>
       </div>
       {/* The div under is a placeholder for the implementation of scrolling balls */}
-      <div id="three-dots" className="flex items-start">
+      <div id="three-dots" className="flex items-start tablet:hidden">
         Scroll Balls
       </div>
     </div>
