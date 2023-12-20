fc7232b0b0d2fa8e15ab8356335b57678fad466a
Andy Lin <76961516+AndyLin910121@users.noreply.github.com>
2023-12-19 17:21:21 -0800

    responsible page size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,18 @@
-import { NavLink, Outlet } from 'react-router-dom';
-import Hamburger from '/public/Hamburger.svg'
+import { NavLink, Outlet } from "react-router-dom";
+import Hamburger from "/public/Hamburger.svg";
+import { useState } from "react";
 
 const Navbar = () => {
-  // const navStyle = {
-  //   background: '#7B8EA6',
-  //   color: '#FFFFFF',
-  //   padding: '20px',
-  //   display: 'flex',
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between',
-  //   alignItems: 'center',
-  // };
-
-  // const ulStyle = {
-  //   listStyle: 'none',
-  //   display: 'flex',
-  //   justifyContent: 'flex-end',
-  // };
-
-  // const liStyle = {
-  //   margin: '0 10px',
-  // };
+  const [isListVisible, setListVisible] = useState(false);
 
   return (
     <div>
       <div className="bg-white text-blue-400 p-4 flex justify-between center ">
         <div className="font-medium">鴻日興科技 First Value Technology Co</div>
-        <ul className="flex list-none justify-end items-center gap-2">
+        <div onClick={() => setListVisible(!isListVisible)}>
+          <img src={Hamburger} className="justify-end center tablet:hidden" />
+        </div>
+        <ul className="hidden tablet:flex list-none justify-end items-center gap-2">
           <NavLink to="/">
             <li>主頁面</li>
           </NavLink>
@@ -39,10 +25,14 @@ const Navbar = () => {
           <li>服務</li>
           <li>聯絡我們</li>
           <li>中文 </li>
-          <li>
-            {/* <div className="bg-hamburger w-5 h-3" /> */}
-            <img src={Hamburger} />
-          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className={`ml-4 z-10 ${isListVisible ? "" : "hidden"}`}>
+          {/* Your list items go here */}
+          <li className="mb-2">Item 1</li>
+          <li className="mb-2">Item 2</li>
+          <li className="mb-2">Item 3</li>
         </ul>
       </div>
 
