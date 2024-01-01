63acc00538524bfa7be0a27ebae16888b6433d7b
BenjaminnHuang <peternick0207@gmail.com>
2023-12-31 21:16:50 -0800

    finished product section and add the photos

--- src/pages/ServiceTech/components/product.jsx
+++ src/pages/ServiceTech/components/product.jsx
@@ -3,10 +3,42 @@ import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 import { TbPointFilled } from 'react-icons/tb';
 import { motion, AnimatePresence } from 'framer-motion';
 import useScreenSize from '../../../components/hooks/useScreenSize';
+import Jig1 from '../../../assets/service/jig_1.png';
+import Jig2 from '../../../assets/service/jig_2.png';
+import Jig3 from '../../../assets/service/jig_3.png';
+import Jig4 from '../../../assets/service/jig_4.png';
+import LED1 from '../../../assets/service/LED_1.png';
+import LED2 from '../../../assets/service/LED_2.png';
+import LED3 from '../../../assets/service/LED_3.png';
+import LED4 from '../../../assets/service/LED_4.png';
+import API1 from '../../../assets/service/API_1.png';
+import API2 from '../../../assets/service/API_2.png';
+import API3 from '../../../assets/service/API_3.png';
+import API4 from '../../../assets/service/API_4.png';
+import Data1 from '../../../assets/service/data_1.png';
+import Data2 from '../../../assets/service/data_2.png';
+import Data3 from '../../../assets/service/data_3.png';
+import Data4 from '../../../assets/service/data_4.png';
+import LightBox1 from '../../../assets/service/lightbox_1.png';
+import LightBox2 from '../../../assets/service/lightbox_2.png';
+import LightBox3 from '../../../assets/service/lightbox_3.png';
+import LightBox4 from '../../../assets/service/lightbox_4.png';
+import Remodel1 from '../../../assets/service/remodel_1.png';
+import Remodel2 from '../../../assets/service/remodel_2.png';
+import Remodel3 from '../../../assets/service/remodel_3.png';
+import Remodel4 from '../../../assets/service/remodel_4.png';
+import System1 from '../../../assets/service/system_1.png';
+import System2 from '../../../assets/service/system_2.png';
+import System3 from '../../../assets/service/system_3.png';
+import System4 from '../../../assets/service/system_4.png';
+import DryIce1 from '../../../assets/service/dryice_1.png';
+import DryIce2 from '../../../assets/service/dryice_2.png';
+import DryIce3 from '../../../assets/service/dryice_3.png';
+import DryIce4 from '../../../assets/service/dryice_4.png';
 
 const products = [
   {
-    name: 'ITO燒結爐',
+    name: '高亮度燈箱',
     description:
       '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
     details: [
@@ -16,35 +48,55 @@ const products = [
       '厚度公差 +/-0.02 mm',
       '尺寸公差 +/-0.05 mm',
     ],
+    photos: [LightBox1, LightBox2, LightBox3, LightBox4],
   },
   {
-    name: 'Product 2',
+    name: 'LED UV固化燈源',
     description:
       '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [LED1, LED2, LED3, LED4],
   },
   {
-    name: 'Product 3',
+    name: '測試治具',
     description:
       '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [Jig1, Jig2, Jig3, Jig4],
   },
   {
-    name: 'Product 4',
+    name: '自動供應系統',
     description:
       '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [System1, System2, System3, System4],
   },
   {
-    name: 'Product 5',
+    name: '設備及機構優化改造',
     description:
       '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [Remodel1, Remodel2, Remodel3, Remodel4],
+  },
+  {
+    name: 'API巡邊檢查/裂片',
+    description:
+      '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [API1, API2, API3, API4],
+  },
+  {
+    name: '監控系統及資料統整',
+    description:
+      '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [Data1, Data2, Data3, Data4],
+  },
+  {
+    name: '乾冰清洗',
+    description:
+      '這總技術有別於一般的梨型狀產品的長晶爐, 其生長過程即是多片狀, 搭配均溫的控制及自動化的提拉重量控制分析',
+    photos: [DryIce1, DryIce2, DryIce3, DryIce4],
   },
 ];
 
 const Product = () => {
-  const [product, setProduct] = useState({
-    name: products[0].name,
-    description: products[0].description,
-    details: products[0].details,
-  });
+  const [product, setProduct] = useState(products[0]);
+  const [selectedPhoto, setSelectedPhoto] = useState(product.photos[0]);
   const [isOpen, setIsOpen] = useState(false);
   const [isDeskTop, setIsDeskTop] = useState(false);
   const screenSize = useScreenSize();
@@ -55,7 +107,7 @@ const Product = () => {
     } else {
       setIsDeskTop(true);
     }
-  }, screenSize);
+  }, [screenSize]);
 
   const menu = {
     initial: {
@@ -108,16 +160,31 @@ const Product = () => {
 
   const renderGallery = () => {
     return (
-      <div className="w-full mt-2">
+      <div className="w-full mt-5">
         {/* Grid container */}
-        <div className="grid grid-cols-3 gap-4">
+        <div className="flex flex-col gap-4 desktop:flex-row">
           {/* Main image placeholder that spans the full width */}
-          <div className="col-span-3 bg-gray-300 rounded-lg h-64 w-full"></div>
+          <img src={selectedPhoto} alt="Selected" className="w-full h-auto" />
 
           {/* Thumbnails placeholders */}
-          <div className="bg-gray-200 rounded-lg h-24 w-full"></div>
-          <div className="bg-gray-200 rounded-lg h-24 w-full"></div>
-          <div className="bg-gray-200 rounded-lg h-24 w-full"></div>
+          <div className="w-full desktop:w-1/3 flex flex-row desktop:flex-col justify-between gap-4">
+            {product.photos.map((image, index) => {
+              if (image === selectedPhoto) {
+                return null;
+              } else
+                return (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Thumbnail ${index + 1}`}
+                    onClick={() => {
+                      setSelectedPhoto(image);
+                    }}
+                    className="w-1/4 tablet:w-3/7 desktop:w-full h-auto"
+                  />
+                );
+            })}
+          </div>
         </div>
       </div>
     );
@@ -137,22 +204,32 @@ const Product = () => {
               {product.name === '' ? 'Select a Product' : product.name}
             </span>
 
-            {isOpen ? <FaAngleUp size={20} /> : <FaAngleDown size={20} />}
+            {isOpen ? (
+              <>
+                <FaAngleUp size={20} />
+              </>
+            ) : (
+              <>
+                <FaAngleDown size={20} />
+              </>
+            )}
           </div>
           {renderOptions()}
         </>
       ) : (
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col">
           {products.map((p) => {
             return (
               <div
+                key={p.details}
                 className={`w-[211px] h-[58px] p-5 ${
                   p.name === product.name
                     ? 'bg-foundation-blue-normal text-white'
                     : 'bg-white'
-                } rounded-[20px] shadow-lg text-center cursor-pointer mt-8`}
+                } rounded-[20px] shadow-lg text-center cursor-pointer mt-8 font-chi-sans`}
                 onClick={() => {
                   setProduct(p);
+                  setSelectedPhoto(p.photos[0]);
                 }}
               >
                 {p.name}
@@ -164,11 +241,11 @@ const Product = () => {
 
       <div className="mt-8 w-full">
         {/* Product Title */}
-        <div className="text-[17px] font-normal font-chi-sans text-foundation-blue-normal text-start">
+        <div className="text-[17px] tablet:text-lg desktop:text-[27px] font-normal font-chi-sans text-foundation-blue-normal text-start">
           {product.name}
         </div>
         {/* Product Description */}
-        <div className="text-[11px] font-normal font-chi-sans text-natural-color-black text-start my-2.5">
+        <div className="text-[11px] tablet:text-sm desktop:text-xl font-normal font-chi-sans text-natural-color-black text-start my-2.5">
           {product.description}
         </div>
 
@@ -177,11 +254,13 @@ const Product = () => {
         {/* Product Details */}
         {product.details && (
           <div className="flex flex-col gap-2 w-full p-4 rounded-lg border-2 mt-5">
-            <div className="text-sm font-medium font-chi-sans">詳細資訊</div>
+            <div className="text-sm desktop:text-lg font-medium font-chi-sans">
+              詳細資訊
+            </div>
             {product.details.map((detail) => {
               return (
                 <div
-                  className="flex flex-row items-center gap-1 text-[11px] font-normal font-chi-sans text-natural-color-black"
+                  className="flex flex-row items-center gap-1 text-[11px] tablet:text-xs desktop:text-lg font-normal font-chi-sans text-natural-color-black"
                   key={detail}
                 >
                   <TbPointFilled size={10} />
