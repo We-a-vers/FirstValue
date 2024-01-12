79b58cde43c893d31b4ead8b37ddc01e499abacc
y00628 <yol043@ucsd.edu>
2024-01-11 18:14:05 -0800

    did some styling changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,67 +5,75 @@ import useScreenSize from './hooks/useScreenSize';
 import { IoClose } from 'react-icons/io5';
 
 const Navbar = () => {
-  const [isListVisible, setListVisible] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const screenSize = useScreenSize();
 
   useEffect(() => {
-    if (screenSize.width > 768) {
-      setListVisible(false);
+    if (screenSize.width < 1440) {
+      setIsDesktop(false);
+    } else {
+      setIsDesktop(true);
     }
   }, [screenSize.width]);
 
   return (
     <div>
-      <div className="bg-white text-blue-400 p-4 flex justify-between items-center">
-        <div className="font-medium">鴻日興科技 First Value Technology Co</div>
+      <div className="bg-white font-chi-sans py-3 px-4 flex justify-between items-center desktop:px-32 desktop:py-7 desktop:text-base">
+        <div className="text-foundation-blue-normal text-base font-normal tablet:text-lg">
+          鴻日興 {isDesktop && '科技 First Value Technology Co'}
+        </div>
         <div>
-          {!isListVisible && (
-            <div onClick={() => setListVisible(!isListVisible)}>
-              <img
-                src={Hamburger}
-                className="justify-end center tablet:hidden"
-              />
-            </div>
-          )}
+          {isDesktop ? (
+            <div>
+              <ul className="flex flex-row gap-4">
+                {/* Your list items go here */}
+                <NavLink to="/">
+                  <li className="mb-2">主頁面</li>
+                </NavLink>
+                <NavLink to="about">
+                  <li className="mb-2">關於我們</li>
+                </NavLink>
+                <NavLink to="service">
+                  <li className="mb-2">技術＆服務</li>
+                </NavLink>
 
-          {isListVisible && (
-            <div onClick={() => setListVisible(!isListVisible)}>
-              <IoClose style={{ fontSize: '2em' }} />
+                <li className="mb-2">聯絡我們</li>
+              </ul>
             </div>
+          ) : (
+            <>
+              {!isOpen && (
+                <div onClick={() => setIsOpen(true)}>
+                  <img src={Hamburger} className="justify-end" />
+                </div>
+              )}
+
+              {isOpen && (
+                <div onClick={() => setIsOpen(false)}>
+                  <IoClose style={{ fontSize: '2em' }} />
+                </div>
+              )}
+            </>
           )}
         </div>
-
-        <ul className="hidden tablet:flex list-none justify-end items-center gap-2">
-          <NavLink to="/">
-            <li>主頁面</li>
-          </NavLink>
-          <NavLink to="about">
-            <li>關於我們</li>
-          </NavLink>
-          <NavLink to="service">
-            <li>技術</li>
-          </NavLink>
-          <li>服務</li>
-          <li>聯絡我們</li>
-          <li>中文 </li>
-        </ul>
       </div>
 
-      {isListVisible ? (
+      {isOpen ? (
         <div>
-          <ul className="ml-4 ">
+          <ul className="text-center mt-8 flex flex-col gap-3 font-chi-sans text-2xl tablet:text-xl text-foundation-blue-normal">
             {/* Your list items go here */}
             <NavLink to="/">
-              <li className="mb-2">主頁面</li>
+              <li>主頁面</li>
             </NavLink>
             <NavLink to="about">
-              <li className="mb-2">關於我們</li>
+              <li>關於我們</li>
             </NavLink>
             <NavLink to="service">
-              <li className="mb-2">技術＆服務</li>
+              <li>技術＆服務</li>
             </NavLink>
 
-            <li className="mb-2">聯絡我們</li>
+            <li>聯絡我們</li>
           </ul>
         </div>
       ) : (
