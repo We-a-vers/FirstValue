f2b9d49988ae39760eb4028e0265650a066cf90f
BenjaminnHuang <peternick0207@gmail.com>
2024-01-13 19:38:44 -0800

    edit the value card component and value section.

--- src/pages/Home/components/ValueCard.jsx
+++ src/pages/Home/components/ValueCard.jsx
@@ -2,30 +2,12 @@ import PropTypes from 'prop-types';
 
 const ValueCard = ({ icon, word }) => {
   return (
-    <div
-      className="flex justify-center items-center w-40 h-36 rounded-xl shadow-value-card backdrop-blur-sm px-3.5 py-6
-                    tablet:w-48 tablet:h-44 tablet:px-4 tablet:py-6
-                    desktop:w-96 desktop:h-64 desktop:px-10 desktop:py-10"
-    >
-      <div
-        className="flex flex-col gap-3 justify-center items-start w-36 h-32
-                            tablet:flex-start tablet:gap-3.5 tablet:w-40 tablet:h-36
-                            desktop:gap-4 desktop:w-auto desktop:h-auto"
-      >
-        <div
-          className=" w-8 h-8
-                            tablet:w-10 tablet:h-10
-                            desktop:w-valueImgSizeDesk desktop:h-valueImgSizeDesk"
-        >
-          <img src={icon} alt="value icon"></img>
-        </div>
-        <div
-          className=" font-chi-sans text-[0.6875rem] not-italic font-normal leading-4
-                            tablet:text-xs
-                            desktop:text-xl desktop:leading-6"
-        >
-          {word}
-        </div>
+    <div className="flex flex-col min-h-48 justify-evenly tablet:max-w-[300px] tablet:h-[181.99px] desktop:max-w-[380px] desktop:h-[260px] py-6 tablet:py-6 px-4 tablet:px-4 gap-3 bg-white rounded-lg">
+      <div>
+        <img src={icon} alt="value icon"></img>
+      </div>
+      <div className="font-chi-sans text-xl tablet:text-xs self-center desktop:text-2xl text-justify font-normal text-black">
+        {word}
       </div>
     </div>
   );
